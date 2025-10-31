8ef124c0efbbc771c49a3343061f8c67a1c99e0f
PaluruSubbaNarasaiah <palurusubbanarasaiah@example.com>
2025-10-31 18:11:11 +0530

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,9 +23,9 @@ export default function Navbar({ onEnrollClick }: NavbarProps) {
              <img 
                 src="/logo.png" 
                 alt="TechSpire Academy Logo" 
-                className="w-12 h-12 mr-3"
+                className="w-59 h-40 mr-5"
               />
-            <h1 className="text-2xl font-bold text-blue-600">Tech Spire Academy</h1>
+            <h1 className="text-2xl mr-2 font-bold text-blue-600">Tech Spire Academy</h1>
           </Link>
 
           <div className="flex items-center space-x-8">
